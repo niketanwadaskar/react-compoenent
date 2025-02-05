cd2e9ed6e9d4e9b5dcbcb937ec3c22858d9edcb4
Niketan <niketanw@mirakitech>
2025-02-05 19:19:53 +0530

    p

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Card from "./component/Card";
 import RadixUi from "./component/RadixUi";
 import "@radix-ui/themes/styles.css";
 import TimelineEffect from "./component/TimelineEffect";
+import ServerSideCompo from "./component/ServerSideCompo";
 function App() {
   return (
     <div className=" gap-12 w-full justify-center flex flex-wrap  items-center">
-      <TimelineEffect />
+      <ServerSideCompo />
     </div>
   );
 }
---
+++ src/component/ServerSideCompo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default async function ServerSideCompo() {
+  return <div>ServerSideCompo:-{typeof window}</div>;
+}
